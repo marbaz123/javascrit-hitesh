97248b70738b7b136fb998589d845a0311a3659f
Arbaz Ali <121857030+marbaz123@users.noreply.github.com>
2024-10-05 18:51:31 +0000

    datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"//treat all JS code as newer version
+
+//alert(3 + 3)  //we are using node js not browser
+
+
+//data types
+
+//number
+//BigInt
+//string
+//boolean
+//null  => standalone value;
+//undefined => value assign nhi hui hai
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof "arbaz"); //string
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
+
+
