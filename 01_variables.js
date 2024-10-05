54a693466f6ea3a25cd7136bb1e1ae3ce2d286d8
Arbaz Ali <121857030+marbaz123@users.noreply.github.com>
2024-10-05 18:28:03 +0000

    for demo

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 44335;
+let email = "arbaz@324.com";
+var password = "raza";
+accountcity = "noida";
+console.log(accountId);
+
+console.table([accountId, email,password,accountcity]);
+/*prefer not to used var 
+because of issue in block scope and functional scope
+*/
